c5dcbb1b4f9eb059befd4ad6dda9137230b47cf3
laisvarela <lais.varelaa@gmail.com>
2025-09-07 23:46:53 -0300

    :sparkles: feat: criação da tela main.dart

--- cinetrack/lib/core/asset_images.dart
+++ cinetrack/lib/core/asset_images.dart
@@ -1,3 +1,5 @@
 class AssetImages {
-  
+  static const String _assetPath = 'assets/images';
+  static const String mainBackground = '$_assetPath/main_background.png';
+  static const String mainIcon = '$_assetPath/popcorn_logo.png';
 }
--- cinetrack/lib/main.dart
+++ cinetrack/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:cinetrack/core/asset_images.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 // cor azul de fundo #191530
 // roxo #361d57
 // botão não selecionado #333a4d
@@ -13,9 +15,122 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('CineTrack'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
+          children: [
+            // primeira camada de fundo: container com textura granulada
+            Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFF191530),
+                image: DecorationImage(
+                  image: AssetImage(AssetImages.mainBackground),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            // camada da frente: container central semi-transparente
+            Center(
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: 20,
+                  left: 20,
+                  right: 20,
+                  bottom: 30,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFcd90f5).withAlpha(50),
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(
+                    color: const Color(0xFFcd90f5),
+                    width: 1.5,
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: Image.asset(
+                        AssetImages.mainIcon,
+                        width: 75,
+                        height: 75,
+                      ),
+                    ),
+                    Text(
+                      'CineTrack',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'Sua jornada cinematográfica começa aqui',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w100,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    SizedBox(
+                      width: 400,
+                      child: FilledButton(
+                        style: FilledButton.styleFrom(
+                          backgroundColor: const Color(0xFFba851a),
+                          side: const BorderSide(
+                            color: Color(0xFFf5b938),
+                            width: 1,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              12,
+                            ), // Altere o valor para o radius desejado
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    SizedBox(
+                      width: 400,
+                      child: FilledButton(
+                        style: FilledButton.styleFrom(
+                          backgroundColor: const Color(0xFFba851a),
+                          side: const BorderSide(
+                            color: Color(0xFFf5b938),
+                            width: 1,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                              12,
+                            ), // Altere o valor para o radius desejado
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: const Text(
+                            'Cadastrar',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
