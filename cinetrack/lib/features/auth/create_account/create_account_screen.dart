cfba5c0524808caf073048dee5243d01a7544e97
laisvarela <lais.varelaa@gmail.com>
2025-09-11 09:24:50 -0300

    :sparkles: automatização da rota Home e implementação do AuthRepository no create_account_screen

--- cinetrack/lib/features/auth/create_account/create_account_screen.dart
+++ cinetrack/lib/features/auth/create_account/create_account_screen.dart
@@ -1,3 +1,8 @@
+import 'package:cinetrack/core/asset_images.dart';
+import 'package:cinetrack/features/auth/repositories/auth_repository.dart';
+import 'package:cinetrack/features/auth/routes/auth_routes.dart';
+import 'package:cinetrack/features/home/routes/home_screen_routes.dart';
+import 'package:cinetrack/routes/web_routes.dart';
 import 'package:flutter/material.dart';
 
 class CreateAccountScreen extends StatefulWidget {
@@ -8,6 +13,11 @@ class CreateAccountScreen extends StatefulWidget {
 }
 
 class _CreateAccountScreenState extends State<CreateAccountScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,6 +42,153 @@ class _CreateAccountScreenState extends State<CreateAccountScreen> {
               color: const Color(0xFFcd90f5).withAlpha(40),
               borderRadius: BorderRadius.circular(12),
               border: Border.all(color: const Color(0xFFcd90f5), width: 0.5),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    AssetImages.mainIcon,
+                    width: 75,
+                    height: 75,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'CineTrack',
+                  style: Theme.of(
+                    context,
+                  ).textTheme.displayLarge?.copyWith(color: Colors.white),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Crie sua conta',
+                  style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Form(
+                  key: _formKey,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 10,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: Text(
+                            'Nome',
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ),
+                        TextFormField(
+                          controller: _nameController,
+                          decoration: InputDecoration(
+                            hintText: 'Digite seu nome',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, digite um nome';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: Text(
+                            'E-mail',
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ),
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            hintText: 'Digite seu e-mail',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, digite um e-mail';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: Text(
+                            'Senha',
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            hintText: 'Digite sua senha',
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor, digite uma senha';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    bool isValid = _formKey.currentState!.validate();
+                    if (!isValid) return;
+                    try {
+                      final authRepository = AuthRepository();
+                      await authRepository.createAccount(
+                        email: _emailController.text,
+                        password: _passwordController.text,
+                        name: _nameController.text,
+                      );
+                      if (!context.mounted) return;
+                      Navigator.of(context).popUntil((route) => route.isFirst);
+                      Navigator.of(context).pushReplacementNamed(HomeScreenRoutes.home);
+                    } on AuthException catch (e) {
+                      if (!context.mounted) return;
+                    }
+                  },
+                  child: Text('Criar conta'),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Já possui uma conta? '),
+                    TextButton(
+                      style: ButtonStyle(
+                        foregroundColor: WidgetStateProperty.all(
+                          Color(0xFFf5b938),
+                        ),
+                        overlayColor: WidgetStateProperty.all(
+                          Colors.transparent,
+                        ),
+                        padding: WidgetStateProperty.all(EdgeInsets.zero),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, AuthRoutes.login);
+                      },
+                      child: Text(
+                        'Faça Login',
+                        style: TextStyle(fontWeight: FontWeight.normal),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
--- cinetrack/lib/features/home/routes/home_screen_routes.dart
+++ cinetrack/lib/features/home/routes/home_screen_routes.dart
@@ -2,7 +2,8 @@ import 'package:cinetrack/features/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreenRoutes {
+  static const String home = '/home';
   static final Map<String, Widget Function(BuildContext)> routes = {
-    '/home': (context) => const HomeScreen(),
+    home: (context) => const HomeScreen(),
   };
 }
