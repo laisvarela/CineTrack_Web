04a96d3343e99589a3a13e6bea7cca8d2cdae147
laisvarela <lais.varelaa@gmail.com>
2025-09-18 19:20:38 -0300

    :sparkles: feat: scraping de titulo, ano, direção, generos e sinopse

--- Bot/scraping.py
+++ Bot/scraping.py
@@ -6,7 +6,7 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
-
+import re
 options = Options()
 options.binary_location = r"C:\Program Files\Mozilla Firefox\firefox.exe"
 geckodriver_path = r"C:\CineTrack_Web\Bot\geckodriver.exe"
@@ -56,20 +56,64 @@ def scraper(link):
     direcao = []
     generos = []
     url_capa = None
-    lista = driver.find_elements(By.TAG_NAME, "ul")
-    for item in lista:
-        item.find_element(By.TAG_NAME, "li")
-        if item.get_attribute("class"):
-            div_principal = item.find_element(By.CSS_SELECTOR, ".card.entity-card.entity-card-list.cf")
-            div_secundaria = div_principal.find_element(By.CLASS_NAME, "meta")
-            titulo = div_secundaria.find_element(By.TAG_NAME, "h2").text
-            div_corpo = div_secundaria.find_elements(By.CLASS_NAME, "meta-body")
-            for div in div_corpo:
-                div.find_element(By.TAG_NAME, "span")
-                if div.get_attribute("class") == "Director":
-                    return
-            
-    return
+    try:
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, "li")))
+        lista = driver.find_elements(By.TAG_NAME, "li")
+        with open(r"C:\CineTrack_Web\Bot\filmes.txt", "r", encoding="utf-8") as read:
+            titulos = set()
+            for linha in read:
+                if linha.startswith("Título:"):
+                    titulo = linha.replace("Título: ", "").replace("\n", "")
+                    titulos.add(titulo)
+                    
+        with open(r"C:\CineTrack_Web\Bot\filmes.txt", "a", encoding="utf-8") as f:
+            for item in lista:
+                if item.get_attribute("class"):
+                    try:
+                        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".card.entity-card.entity-card-list.cf")))
+                        div_principal = item.find_element(By.CSS_SELECTOR, ".card.entity-card.entity-card-list.cf")
+                    except:
+                        continue
+                    try:
+                        linhas = div_principal.text.split("\n")
+                        if linhas and len(linhas) >= 2:
+                            titulo = ' '.join(linhas[0].split())
+                            partes = [p.strip() for p in linhas[1].split("|")]
+                            if len(partes) >= 3:
+                                match = re.search(r"\b\d{4}\b", partes[0])
+                                ano = match.group(0) if match else None
+                                generos = ', '.join([g.strip() for g in partes[2].split(",")])
+                            else:
+                                ano = 0
+                                generos = []
+                                # Procura pela linha de direção
+                            direcao = []
+                            
+                            if linhas[2].lower().startswith("direção:"):
+                                direcao = ', '.join([d.strip() for d in linhas[2].replace("Direção:", "").split(",")])
+                            
+                            
+                            if len(linhas) > 1:
+                                # Ignora o título (linhas[0]) e pega a maior das demais
+                                sinopse = max(linhas[1:], key=len).replace(',', '')
+                            else:
+                                sinopse = "Sem sinopse."
+
+                            if titulo not in titulos:
+                                try:
+                                    f.write(f"Título: {titulo}\nAno: {ano}\nGênero: {generos}\nDireção: {direcao}\nSinopse: {sinopse}\n\n")
+                                    titulos.add(titulo.replace("\n", ""))
+                                except Exception as e: 
+                                    print(f"Erro ao salvar filme. Erro: {e}")
+                                    continue
+                            
+                    except Exception as e:
+                        print(f"Falha no scraping. Erro {e}")
+                        
+    except Exception as e:
+        print(f"Falha ao encontrar elementos na página. Erro: {e}")
+        return False
+    return True
 
 if __name__ == "__main__":
     crawler()
