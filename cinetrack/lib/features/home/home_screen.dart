9bb03843b55c493004fb5a32d854ca3e6d5a0715
laisvarela <lais.varelaa@gmail.com>
2025-10-09 08:29:51 -0300

    :sparkles: feat: criação do repositório de usuário com método para obter o papel do usuário

--- cinetrack/lib/features/home/home_screen.dart
+++ cinetrack/lib/features/home/home_screen.dart
@@ -1,12 +1,19 @@
 import 'package:cinetrack/core/asset_images.dart';
 import 'package:cinetrack/features/auth/routes/auth_routes.dart';
 import 'package:cinetrack/theme/web_theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ cinetrack/lib/features/user/repositories/user_repository.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserRepository {
+  String? userRole;
+  Future<void> getUserRole() async {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid != null) {
+      final doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(uid)
+          .get();
+      userRole = doc.data()?['role'];
+    }
+  }
+}
