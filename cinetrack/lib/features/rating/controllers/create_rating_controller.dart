40cdc3ab114307c0abe937affb083811649bad91
laisvarela <lais.varelaa@gmail.com>
2025-11-17 09:42:05 -0300

    :sparkles: feat: Implement CreateRatingController for handling rating creation logic

---
+++ cinetrack/lib/features/rating/controllers/create_rating_controller.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:cinetrack/features/rating/models/create_rating_model.dart';
+import 'package:cinetrack/features/rating/repositories/rating_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final createRatingProvider =
+    AsyncNotifierProvider.autoDispose<CreateRatingController, void>(
+      CreateRatingController.new,
+    );
+
+class CreateRatingController extends AutoDisposeAsyncNotifier<void> {
+  @override
+  FutureOr<dynamic> build() {
+    return null;
+  }
+
+  void create(CreateRatingModel rating) async {
+    try {
+      state = AsyncValue.loading();
+      await RatingRepository().createRating(rating: rating);
+      state = AsyncValue.data(null);
+    } catch (error, stackTrace) {
+      state = AsyncValue.error(error, stackTrace);
+    }
+  }
+}
