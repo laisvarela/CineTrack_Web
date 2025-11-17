c0e58b28c202da1572e3ea0f39e92c6ab7078717
laisvarela <lais.varelaa@gmail.com>
2025-11-17 11:13:03 -0300

    :art: style: Remove commented color codes and unnecessary comments in main.dart for cleaner code

--- cinetrack/lib/features/rating/controllers/create_rating_controller.dart
+++ cinetrack/lib/features/rating/controllers/create_rating_controller.dart
@@ -19,6 +19,7 @@ class CreateRatingController extends AutoDisposeAsyncNotifier<void> {
     try {
       state = AsyncValue.loading();
       await RatingRepository().createRating(rating: rating);
+      // como o provider é do tipo null, o success é sinalizado com null também
       state = AsyncValue.data(null);
     } catch (error, stackTrace) {
       state = AsyncValue.error(error, stackTrace);
--- cinetrack/lib/main.dart
+++ cinetrack/lib/main.dart
@@ -16,15 +16,8 @@ void main() async {
   runApp(ProviderScope(child: const MyApp()));
 }
 
-
-// cor azul de fundo #191530
-// roxo #361d57
-// botão não selecionado #333a4d
-// botão selecionado #ba851a
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
