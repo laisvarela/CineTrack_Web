ed54cd62a92431fb2db5da8546a026032deccfdf
laisvarela <lais.varelaa@gmail.com>
2025-11-24 21:29:42 -0300

    Deploy web build to repo root

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "23caee0fa08663241ffc9ae73ab7c6d8
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "3b85f3f84e22ed3978c13637185217f2",
+"flutter_bootstrap.js": "e991d3b70c23b440c253b626989530a3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
