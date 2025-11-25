368a0b7ec1abbf9df615b86fe64613921d2a835f
laisvarela <lais.varelaa@gmail.com>
2025-11-24 23:55:33 -0300

    :bug: fix: Add missing file extensions for icon asset paths in AssetImages class

--- cinetrack/lib/core/asset_images.dart
+++ cinetrack/lib/core/asset_images.dart
@@ -2,6 +2,6 @@ class AssetImages {
   static const String _assetPath = 'assets/images';
   static const String mainBackground = '$_assetPath/main_background.png';
   static const String mainIcon = '$_assetPath/popcorn_logo.png';
-  static const String iconStar = '$_assetPath/icon_star';
-  static const String iconHalfStar = '$_assetPath/icon_half_star';
+  static const String iconStar = '$_assetPath/icon_star.svg';
+  static const String iconHalfStar = '$_assetPath/icon_half_star.svg';
 }
